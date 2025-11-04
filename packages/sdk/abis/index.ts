d2a4d2a01074bbddf97cfe128583e310b817363e
llamalm <195907590+llamalm@users.noreply.github.com>
2025-11-04 10:40:30 -0300

    update abis and deployments

--- packages/sdk/abis/index.ts
+++ packages/sdk/abis/index.ts
@@ -6,6 +6,8 @@ import UMAResolver from './UMAResolver.json';
 import CollateralToken from './CollateralToken.json';
 import Foil from './Foil.json';
 import FoilFactory from './FoilFactory.json';
+import LZResolver from './LZResolver.json';
+import LZResolverUmaSide from './LZResolverUmaSide.json';
 
 export const predictionMarketAbi: Abi = (PredictionMarket as { abi: Abi }).abi;
 export const liquidityVaultAbi: Abi = (LiquidityVault as { abi: Abi }).abi;
@@ -13,3 +15,5 @@ export const umaResolverAbi: Abi = (UMAResolver as { abi: Abi }).abi;
 export const collateralTokenAbi: Abi = (CollateralToken as { abi: Abi }).abi;
 export const foilAbi: Abi = (Foil as { abi: Abi }).abi;
 export const foilFactoryAbi: Abi = (FoilFactory as { abi: Abi }).abi;
+export const lzResolverAbi: Abi = (LZResolver as { abi: Abi }).abi;
+export const lzResolverUmaSideAbi: Abi = (LZResolverUmaSide as { abi: Abi }).abi;
