e6c66946d8478a2183f0faa88fbfaadbd50d444c
Zayd <zaydapikeys@gmail.com>
2025-11-04 17:53:55 -0800

    init

--- packages/sapience/src/components/home/Ticker.tsx
+++ packages/sapience/src/components/home/Ticker.tsx
@@ -2,14 +2,35 @@
 
 import * as React from 'react';
 import FeaturedMarketGroupCards from './FeaturedMarketGroupCards';
+import { useConditions } from '~/hooks/graphql/useConditions';
 
 export default function Ticker() {
   const [isMounted, setIsMounted] = React.useState(false);
+  const { data: conditions, isLoading } = useConditions({
+    take: 100,
+  });
 
   React.useEffect(() => {
     const id = requestAnimationFrame(() => setIsMounted(true));
     return () => cancelAnimationFrame(id);
   }, []);
+
+  // Don't render anything while loading or if there are no active public conditions
+  if (isLoading) {
+    return null;
+  }
+
+  const now = Math.floor(Date.now() / 1000);
+  const hasActiveConditions = conditions?.some((c) => {
+    if (typeof c.endTime !== 'number' || c.endTime <= 0) return false;
+    if (!c.public) return false;
+    return now <= c.endTime;
+  });
+
+  // Hide the entire ticker section if there are no active conditions
+  if (!hasActiveConditions) {
+    return null;
+  }
 
   return (
     <div
