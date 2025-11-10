3381581889d5252e98d1c17ef08b9522a31a99f3
Matías <mjlescano@protonmail.com>
2025-11-10 15:01:10 -0300

    add sentry environment

--- packages/api/src/sentry.ts
+++ packages/api/src/sentry.ts
@@ -4,6 +4,7 @@ import { config } from './config';
 if (config.SENTRY_DSN) {
   Sentry.init({
     dsn: config.SENTRY_DSN,
+    environment: config.NODE_ENV,
     tracesSampleRate: 1.0,
     sendDefaultPii: true,
   });
